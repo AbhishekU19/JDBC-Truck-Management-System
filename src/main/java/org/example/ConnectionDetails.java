e09c5982714bd131b04aad89f879be654ed5c1de
Your Name <your-email@example.com>
2025-01-12 22:38:24 +0530

    Initial commit

---
+++ src/main/java/org/example/ConnectionDetails.java
@@ -0,0 +1,18 @@
+package org.example;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDetails  {
+    public static String  LOAD_DRIVER="com.mysql.cj.jdbc.Driver";
+    public static String  URL="jdbc:mysql://localhost:3306/truckmgt";
+    public static String  PASSWORD="Mysql@25";
+    public static String  USERNAME="root";
+
+
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(URL,USERNAME,PASSWORD);
+    }
+
+}
+
